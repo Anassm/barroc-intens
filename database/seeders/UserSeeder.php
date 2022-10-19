3679c055107662e4bba028e4646238e504895171
Anass Moussadi <a.moussadi03@gmail.com>
2022-10-19 10:57:10 +0200

    Styling & Database Seeders + migrations

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Stringable;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        # --JETSTREAM GEBRUIKERS--
+        # --Anass--
+        DB::table('users')->insert([
+            'name' => "Anass",
+            'email' => "testAnass@test.nl",
+            'password' => '$2y$10$ZFX6ucM/vo/f5lsvpA9RteGQxOQzH.qXqFMcAJmeqoIeAnWBCZQ2W',
+            'current_team_id' => '4',
+        ]);
+        # --Robin--
+        DB::table('users')->insert([
+            'name' => "Robin",
+            'email' => "testRobin@test.nl",
+            'password' => '$2y$10$PfsPKXwJSzMDUTOS3OgsXuXnafzYEEWu.Il.N5PKHs5UP/Ys0d5RK',
+            'current_team_id' => '6',
+        ]);
+        # --Gino--
+        DB::table('users')->insert([
+            'name' => "Gino",
+            'email' => "testGino@test.nl",
+            'password' => '$2y$10$awYd20ln4BfY/CELc5CSGOA0HhJ99DE.4TZP94QIvxokesPlwlIp6',
+            'current_team_id' => '7',
+        ]);
+    }
+}
