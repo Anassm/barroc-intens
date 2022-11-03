f06c3ff47741bc85c77dae072efd5daa8da9cc02
Anass Moussadi <a.moussadi03@gmail.com>
2022-11-03 10:00:37 +0100

    init

--- resources/views/maintenance/storing.blade.php
+++ resources/views/maintenance/storing.blade.php
@@ -15,6 +15,16 @@
     </header>
 
     <h1 class="text-4xl pl-48 pt-16 font-semibold">Overzicht storingen</h1>
+    <table>
+        <tr class="">
+            <th class="text-left pt-6 pl-6">Status</th>
+            <th class="text-left pt-6 pl-6">Naam</th>
+        </tr>
+        <tr>
+            <td class="text-left pt-6 pl-6">Submitted</td>
+            <td><a href="#" class="text-left pt-6 pl-6" mb-5    >Een link van een storing</a></td>
+        </tr>
+    </table>
 
     <footer>
         @include("homepagina.footer")
