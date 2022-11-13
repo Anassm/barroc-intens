c0bb2f7188f8b315e89466c3ecb85f4501f20598
Gino Hanemaaijer <g.hanemaaijer11@gmail.com>
2022-11-13 22:14:25 +0100

    aangepast

--- resources/views/components/menusales.blade.php
+++ resources/views/components/menusales.blade.php
@@ -3,3 +3,8 @@
 :active="request()->routeIs('sales.notes.sales')">
 Notes
 </x-jet-nav-link>
+
+<x-jet-nav-link class="text-amber-300  hover:text-amber-200  hover:border-amber-300" href="{{ route('sales.nieuwklant.sales') }}"
+:active="request()->routeIs('sales.nieuwklant.sales')">
+Notes
+</x-jet-nav-link>
